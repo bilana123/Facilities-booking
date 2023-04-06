25f79fa0a5a98197579ebce85a0ed47d53e5fb2f
NetenDema <05210231.jnec@rub.edu.bt>
2023-04-06 18:05:24 +0600

    footer

--- src/Component/Pages/Footer.js
+++ src/Component/Pages/Footer.js
@@ -4,13 +4,13 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="page-footer font-big white pt-4">
-    <div className="container-fluid ">
+    <div className="container-fluid text-center ">
       <div className="row">
         <div className="col mt-3 bg-dark">
-          <h5 className="text-uppercase">WEBMASTER</h5>
+          <h5 className="text-uppercase text-center">WEBMASTER</h5>
           <li>
-            <a href="#!">Email:05210217.jnec@rub.edu.bt </a><br></br>
-            <a href="#!">Phone:+975- </a>
+            <a className="text-center " href="#!">Email:05210217.jnec@rub.edu.bt </a><br></br>
+            <a className="text-center fa fa-phone fa-lg" href="#!">Phone:+975- </a>
             
           </li>
           <h5>Phone:+975-77271922</h5> //
