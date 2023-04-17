1ba6f0eb835ae756732a8793ec0996abd6a58d4e
NetenDema <05210231.jnec@rub.edu.bt>
2023-04-17 10:24:26 +0600

    Committing local changes to Footer.js

--- src/Component/Pages/Footer.js
+++ src/Component/Pages/Footer.js
@@ -4,7 +4,8 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="page-footer font-big white pt-4">
-    <div className="container-fluid text-center ">
+      <div className="md:flex md:justify-between md:items-center sm:px-4 bg-[#ffffff19] py-7 ">
+      <h1 className="lg:text-4x1 text-3x1 md:mb-0 mb:6 lg:leading-normal font-semibold md:w-2/5"><span className="text-teal-400">Free</span>untill you're ready to launch</h1>
       <div className="row">
         <div className="col mt-3 bg-dark">
           <h5 className="text-uppercase text-center">WEBMASTER</h5>
