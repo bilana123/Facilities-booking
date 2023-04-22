ffbc325ca4104c1cc9871f5032b57a15b7988372
bilana123 <05210217jnec@rub.edu.bt>
2023-04-22 23:26:25 +0600

    sorry

--- src/Component/logincomp/Login.js
+++ src/Component/logincomp/Login.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { useLogin } from "../../Hooks/useLogin";
-import "./login.css";
+import "./Login.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setemail] = useState("");
   const [password, setPassword] = useState("");
-  const { login, error, ispending } = useLogin();
+  const { login, logout, error, ispending, isLoggedIn } = useLogin();
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -15,44 +15,72 @@ export default function Login() {
     login(email, password);
     navigate("/admin");
   };
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  const renderLoginForm = () => {
+    return (
+      <>
+        <form onSubmit={handleSubmit} className="Login-form mt-5 rounded-2">
+          <h5>Admin</h5>
+          <label>
+            <span>email:</span>
+            <input
+              type="email"
+              onChange={(e) => setemail(e.target.value)}
+              value={email}
+              style={{ border: "2px solid black" }}
+            />
+          </label>
+
+          <label>
+            <span> password:</span>
+            <input
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              style={{ border: "2px solid black" }}
+            />
+          </label>
+          <br></br>
+
+          {!isLoggedIn && !ispending && (
+            <button className="btn btn-info justify-centre" type="submit">
+              Login
+            </button>
+          )}
+          {ispending && (
+            <button className="btn" disabled>
+              loading
+            </button>
+          )}
+          {error && <p>{error}</p>}
+        </form>
+        <p>
+          Don't have an account?{" "}
+          <Link to="/signup" className="link">
+            Sign Up
+          </Link>
+        </p>
+      </>
+    );
+  };
+
+  const renderLogoutButton = () => {
+    return (
+      <button onClick={handleLogout} className="btn btn-info justify-centre">
+        Logout
+      </button>
+    );
+  };
+
   return (
     <div className="Login-container">
-      <form onSubmit={handleSubmit} className="Login-form mt-5 rounded-2">
-        <h5>Admin</h5>
-        <label>
-          <span>email:</span>
-          <input
-            type="email"
-            onChange={(e) => setemail(e.target.value)}
-            value={email}
-            style={{ border: "2px solid black" }}
-          />
-        </label>
-
-        <label>
-          <span> password:</span>
-          <input
-            type="password"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            style={{ border: "2px solid black" }}
-          />
-        </label>
-        <br></br>
-
-        {!ispending && (
-          <Link to="/admin" className="btn btn-info justify-centre">
-            Login
-          </Link>
-        )}
-        {ispending && (
-          <button className="btn" disabled>
-            loading
-          </button>
-        )}
-        {error && <p>{error}</p>}
-      </form>
+      {isLoggedIn ? renderLogoutButton() : renderLoginForm()}
     </div>
   );
 }
