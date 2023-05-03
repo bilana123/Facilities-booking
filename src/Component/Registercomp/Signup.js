f7be8f8242e1019d5b1e4235fe5849c1072f14cd
Bilina123 <05210217.jnec@rub.edu.bt>
2023-05-03 11:12:41 +0530

    new registration

--- src/Component/Registercomp/Signup.js
+++ src/Component/Registercomp/Signup.js
@@ -4,120 +4,124 @@ import { useSignup } from "../../Hooks/useSignup";
 import { Link } from "react-router-dom";
 import "./Register.css";
 
-export default function Signup() {
-  const [email, setemail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setusername] = useState("");
-  const [department, setDepartment] = useState("");
-  const [Confirm_password, setConfirm_password] = useState("");
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+    department: "",
+    confirm_password: "",
+  });
+
+  const { email, password, username, department, confirm_password } = formData;
+
   const { Signup, isPending, error } = useSignup();
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (Confirm_password === password) {
-      try {
-        Signup(email, password, username, department).then(() => {
-          navigate("/Login");
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      alert("password should match");
+    if (confirm_password !== password) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      await Signup(email, password, username, department);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
     }
   };
 
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
-    <div className="container ">
-      <form onSubmit={handleSubmit} className="Login-form">
-        <h5 className="form-heading">Admin_Register</h5>
-        <label className="form-label">
-          <span className="form-span">Username:</span>
-          <input
-            type="username"
-            required
-            onChange={(e) => setusername(e.target.value)}
-            value={username}
-            className="form-input"
-          />
-        </label>
-        <label className="form-label">
-          <span
-            className={`form-span ${email && "form-span--bold"}`}
-            // add form-span--bold class if email is not empty
-          >
-            Email:
-          </span>
-          <input
-            type="email"
-            onChange={(e) => setemail(e.target.value)}
-            value={email}
-            placeholder="Enter your email"
-            required
-            className="form-input"
-          />
-        </label>
-        <label className="form-label">
-          <span
-            className={`form-span ${department && "form-span--bold"}`}
-            // add form-span--bold class if department is not empty
-          >
-            Department:
-          </span>
-          <select
-            value={department}
-            onChange={(e) => setDepartment(e.target.value)}
-            className="form-input"
-            required
-          >
-            <option value="">Select department</option>
-            <option value="IT">IT</option>
-            <option value="sports">sports</option>
-            <option value="HR ">HR</option>
-          </select>
-        </label>
-        <label className="form-label">
-          <span
-            className={`form-span ${password && "form-span--bold"}`}
-            // add form-span--bold class if password is not empty
-          >
-            Password:
-          </span>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder="Enter password"
-            required
-            className="form-input"
-          />
-        </label>
-        <label className="form-label">
-          <span
-            className={`form-span ${Confirm_password && "form-span--bold"}`}
-            // add form-span--bold class if Confirm_password is not empty
-          >
-            Confirm_password :
-          </span>
-          <input
-            type="Password"
-            onChange={(e) => setConfirm_password(e.target.value)}
-            value={Confirm_password}
-            placeholder="Confirm password"
-            required
-            className="form-input"
-          />
-        </label>
-        <button type="submit" className="form-btn" disabled={isPending}>
-          {isPending ? "Signing up..." : "Sign up"}
-        </button>
-        {error && <p className="error">{error}</p>}
-        <p className="login">
-          Already have an account? <Link to="/login">Log in</Link>
-        </p>
-      </form>
+    <div className="d-flex justify-content-center align-items-center ">
+      <div className="bg-white shadow-lg p-5">
+        <div className="col-lg-12 col-md-5">
+          <form onSubmit={handleSubmit} class="from-login">
+            <h2 className="text-center mb-4">Register</h2>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                required
+                className="form-control"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                required
+                className="form-control"
+                placeholder="Enter username"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="department">Department</label>
+              <select
+                name="department"
+                value={department}
+                onChange={handleChange}
+                required
+                className="form-control"
+              >
+                <option value="">Select department</option>
+                <option value="IT">IT</option>
+                <option value="Sports">Sports</option>
+                <option value="HR">HR</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                required
+                className="form-control"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirm_password">Confirm Password</label>
+              <input
+                type="password"
+                name="confirm_password"
+                value={confirm_password}
+                onChange={handleChange}
+                required
+                className="form-control"
+                placeholder="Confirm password"
+              />
+            </div><br></br>
+            <button
+              type="submit"
+              className="btn btn-primary btn-block"
+              disabled={isPending}
+            >
+              {isPending ? "Signing up..." : "Sign up"}
+            </button>
+            {error && <div className="text-danger mt-3">{error}</div>}
+            <p className="text-center mt-3">
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Signup;
